b2d101a9ef5adfbaffb44673debfb64c7f0cf25c
Hardik Verma <42641528+ItsHardik@users.noreply.github.com>
2018-11-26 14:15:15 +0530

    Add files via upload
    temp data

---
+++ temp/specs/api.spec.ts
@@ -0,0 +1,28 @@
+var supertest = require("supertest");
+var should = require("should");
+
+var server = supertest.agent("http://dev.authorify.com/afy-api-demo");
+
+// UNIT test begin
+
+describe("SAMPLE unit test",function(){
+
+  // #1 should return home page
+
+  it("should return home page",function(done){
+
+    // calling home page api
+    server
+    .get("/draft?draft_id=5beec2ef2340986431bb09e1")
+    .expect("Content-type",/json/)
+    .expect(200) // THis is HTTP response
+    .end(function(err,res){
+      // HTTP status should be 200
+      res.status.should.equal(200);
+      // Error key should be false.
+      res.body.error.should.equal(false);
+      done();
+    });
+  });
+
+});
